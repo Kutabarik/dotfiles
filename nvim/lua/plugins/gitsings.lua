e8ed3d835ef79272a09513e189e986adc760062e
kutabarik <malay.misha@gmail.com>
2025-04-20 10:23:09 +0300

    fix(gitsings): udpate signs icons

--- nvim/lua/plugins/gitsings.lua
+++ nvim/lua/plugins/gitsings.lua
@@ -2,18 +2,12 @@ return {
 	"lewis6991/gitsigns.nvim",
 	opts = {
 		signs = {
-			add = { text = "+" },
-			change = { text = "~" },
-			delete = { text = "_" },
-			topdelete = { text = "‾" },
-			changedelete = { text = "~" },
-		},
-		signs_staged = {
-			add = { text = "+" },
-			change = { text = "~" },
-			delete = { text = "_" },
-			topdelete = { text = "‾" },
-			changedelete = { text = "~" },
+			add = { text = "▐" },
+			change = { text = "▐" },
+			delete = { text = "▐" },
+			topdelete = { text = "▐" },
+			changedelete = { text = "▐" },
+			untracked = { text = "▐" },
 		},
 		current_line_blame = true,
 		current_line_blame_opts = {
